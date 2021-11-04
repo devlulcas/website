0892c0541294041946ec739bb795ca362eb4cee4
devlulcas <lucasalvesrego.contato@gmail.com>
2021-11-04 10:32:53 -0300

    add: basic layout and general styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,58 @@
+<!--JAVASCRIPT-->
+<script>
+</script>
+
+<!--HEAD-->
+<svelte:head>
+	<!--FONTS-->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<!--BODY-->
+<div class="container">
+	<slot />
+</div>
+
+<!--CSS-->
+<style>
+	/* RESET DE CSS */
+	:global(*) {
+		padding: 0;
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	:global(ul, ol) {
+		list-style: none;
+	}
+
+	:global(a) {
+		text-decoration: none;
+	}
+
+	.container {
+		height: 100vh;
+	}
+
+	/* TEMA DE COR */
+	:root {
+		font-size: 62.5%;
+		--white: #fff;
+		--black: #18191f;
+		--blue: #1947e5;
+	}
+
+	/* TIPOGRAFIA */
+	:global(h1, h2, a) {
+		font-family: 'Montserrat', sans-serif;
+	}
+
+	:global(p) {
+		font-family: 'Courier New', Courier, monospace;
+	}
+</style>
