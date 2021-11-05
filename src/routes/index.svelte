5881b7eb30b9e9569afbbeb1e1cf92633a34ed3d
devlulcas <lucasalvesrego.contato@gmail.com>
2021-11-05 05:08:38 -0300

    add: skill tiles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,17 @@
 
 <!--BODY-->
 <main>
-	<div class="herobanner" />
+	<div class="herobanner">
+		<div class="tile"><i class="fab fa-html5" /></div>
+		<div class="tile"><i class="fab fa-css3-alt" /></div>
+		<div class="tile"><i class="fab fa-js-square" /></div>
+		<div class="tile"><i class="fab fa-node-js" /></div>
+		<div class="tile"><i class="fab fa-android" /></div>
+		<div class="tile"><i class="fab fa-linux" /></div>
+		<div class="tile"><i class="fab fa-figma" /></div>
+		<div class="tile"><i class="fab fa-git-alt" /></div>
+		<div class="tile"><span>kotlin</span></div>
+	</div>
 	<section>
 		<h1>Web/Mobile developer</h1>
 		<p>
@@ -33,10 +43,44 @@
 	.herobanner {
 		background: url('../../static/images/heroImage.jpg') center center;
 		background-size: cover;
+		display: grid;
+		gap: 1rem;
+		padding: 1rem;
+		grid-template-rows: 1fr 1fr 1fr;
+		grid-template-columns: 1fr 1fr 1fr;
 	}
 
+	.tile {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		border-radius: 0.3rem;
+		border: 0.08rem solid hsla(0, 0%, 100%, 0.25);
+		background-color: hsla(0, 0%, 100%, 0.15);
+		backdrop-filter: blur(1rem);
+		opacity: 0;
+		transition: opacity 0.3s ease 0.4s;
+	}
+
+	.tile:hover {
+		opacity: 1;
+		transition: opacity 0.2s ease;
+	}
 	section {
 		padding: 2vh 2vw;
+	}
+
+	i,
+	span {
+		color: var(--white);
+		font-size: 5rem;
+	}
+
+	span {
+		font-size: 3.5rem;
+		font-weight: 600;
 	}
 
 	h1 {
@@ -61,6 +105,9 @@
 			grid-template-rows: 1fr;
 		}
 
+		.herobanner {
+			padding-bottom: 13.5vh;
+		}
 		section {
 			padding: 3vh 4vw;
 		}
