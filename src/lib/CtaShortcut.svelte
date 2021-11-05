0beacec0d159df72527ab1dedd7c793f0342e5c1
devlulcas <lucasalvesrego.contato@gmail.com>
2021-11-05 03:51:25 -0300

    add: call to action for homepage

---
+++ src/lib/CtaShortcut.svelte
@@ -0,0 +1,45 @@
+<!--HTML-->
+<nav>
+	<div>
+		<a sveltekit:prefetch href="/contact">Let's talk</a>
+	</div>
+	<p>or</p>
+	<div>
+		<a sveltekit:prefetch href="/projects">first see what I've done</a>
+	</div>
+</nav>
+
+<!--CSS-->
+<style>
+	nav {
+		margin: 10vh 0;
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+
+	a {
+		width: fit-content;
+		position: relative;
+		font-size: 1.3rem;
+		color: var(--blue);
+	}
+
+	a::after {
+		content: '';
+		display: block;
+		width: 0;
+		height: 0.2rem;
+		background: var(--blue);
+		position: absolute;
+		transition: all 0.4s ease-in-out;
+	}
+
+	div:hover a::after {
+		width: 100%;
+	}
+
+	p {
+		font-size: 1.3rem;
+	}
+</style>
