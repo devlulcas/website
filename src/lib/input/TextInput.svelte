cabf069583564dc5899706c21a3e91011f6525d5
devlulcas <lucasalvesrego.contato@gmail.com>
2021-11-06 23:23:51 -0300

    add: input elements

---
+++ src/lib/input/TextInput.svelte
@@ -0,0 +1,15 @@
+<!--JAVASCRIPT-->
+<script>
+	import './Input.css';
+	export let name;
+	export let label;
+	export let icon;
+	export let value;
+</script>
+
+<!--HTML-->
+<div class="input">
+	<label class="sr-only" for={name}>{label}</label>
+	<i class="input-icon {icon}" />
+	<input class="input-field" type="text" {name} id={name} bind:value />
+</div>
