e17b7afbbe810e42e02817bb40209638072180ce
devlulcas <lucasalvesrego.contato@gmail.com>
2021-11-09 18:56:46 -0300

    add: gh pages config

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+	'build',
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/devlulcas/devlulcas.github.io',
+		user: {
+			name: 'Lucas Alves Rego',
+			email: 'lucasalvesrego.contato@gmail.com'
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
