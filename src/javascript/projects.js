89fea1f5e00d64e63c17d507e1237bac2350f2fe
devlulcas <lucasalvesrego.contato@gmail.com>
2021-12-15 20:26:13 -0300

    add: file with all projects data... i know its horrible

---
+++ src/javascript/projects.js
@@ -0,0 +1,40 @@
+class Project {
+	constructor(name, description, thumb, thumbDescription, liveUrl, codeUrl, moreInfoUrl, techs) {
+		this.name = name;
+		this.description = description;
+		this.previewImage = {
+			src: thumb,
+			description: thumbDescription
+		};
+		this.urls = {
+			liveUrl: liveUrl,
+			codeUrl: codeUrl,
+			moreInfoUrl: moreInfoUrl
+		};
+		this.techs = techs;
+	}
+}
+
+// Instâncias de objetos de projeto (HORRENDA FORMA DE FAZER ISSO, MAS É O QUE TEMOS PARA HOJE)
+const portfolio = new Project(
+	'Portfólio',
+	'My personal website',
+	'/images/heroImage.jpg',
+	'prints of my portfolio',
+	'https://devlulcas.github.io',
+	'https://github.com/devlulcas/devlulcas.github.io',
+	'aaa',
+	['HTML', 'CSS', 'JS', 'SVELTE', 'SVELTEKIT', 'GHPAGES']
+);
+
+// Lista dos objetos de projeto
+export const projects = [
+	portfolio,
+	portfolio,
+	portfolio,
+	portfolio,
+	portfolio,
+	portfolio,
+	'aaa',
+	'bb'
+].slice(0, 6);
