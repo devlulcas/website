5dbd36c50319489b03002cfb3ddc30838a5028d6
devlulcas <lucasalvesrego.contato@gmail.com>
2021-12-16 18:58:47 -0300

    add: new projects

--- src/javascript/projects.js
+++ src/javascript/projects.js
@@ -3,7 +3,7 @@ class Project {
 		this.name = name;
 		this.description = description;
 		this.previewImage = {
-			src: thumb,
+			src: "/images/screenshots/" + thumb,
 			description: thumbDescription
 		};
 		this.urls = {
@@ -16,10 +16,21 @@ class Project {
 }
 
 // Instâncias de objetos de projeto (HORRENDA FORMA DE FAZER ISSO, MAS É O QUE TEMOS PARA HOJE)
+const placeholder = new Project(
+	'Placeholder',
+	'A place for a future project',
+	'placeholder.jpg',
+	'nothing here yet',
+	'/notonline',
+	'https://github.com/devlulcas/',
+	'/notonline',
+	['NOTHING', 'NOTHING', 'NOTHING', 'NOTHING', 'NOTHING']
+);
+
 const portfolio = new Project(
 	'Portfólio',
 	'My personal website',
-	'/images/heroImage.jpg',
+	'portfolio.png',
 	'prints of my portfolio',
 	'https://devlulcas.github.io',
 	'https://github.com/devlulcas/devlulcas.github.io',
@@ -27,12 +38,45 @@ const portfolio = new Project(
 	['HTML', 'CSS', 'JS', 'SVELTE', 'SVELTEKIT', 'GHPAGES']
 );
 
+const badgeGenerator = new Project(
+	'Badge generator',
+	'Badges for your readme',
+	'/badgegenerator_screenshot.png',
+	'prints of badge generator',
+	'https://devlulcas.github.io/badge-generator/',
+	'https://github.com/devlulcas/badge-generator',
+	'/notonline',
+	['HTML', 'CSS', 'JS']
+);
+
+const mySecretDiary = new Project(
+	'My Secret Diary',
+	'An online social media and diary',
+	'/mysecretdiary_screenshot.png',
+	'my secret diary homepage',
+	'/notonline',
+	'https://github.com/devlulcas/diary-front',
+	'/notonline',
+	['HTML', 'CSS', 'JS', 'SVELTE', 'NODE', 'KNEX', 'POSTGRES']
+);
+
+const myPersonalStartpage = new Project(
+	'Personal startpage',
+	'Custom browser startpage',
+	'/startpage_screenshot.png',
+	'my personal startpage  with a todo, weather and notes app',
+	'https://devlulcas.github.io/personal-startpage/',
+	'https://github.com/devlulcas/personal-startpage',
+	'/notonline',
+	['HTML', 'CSS', 'JS', 'WEATHER BIT API']
+);
+
 // Lista dos objetos de projeto
 export const projects = [
 	portfolio,
-	portfolio,
-	portfolio,
-	portfolio,
-	portfolio,
-	portfolio,
+	badgeGenerator,
+	mySecretDiary,
+	myPersonalStartpage,
+	placeholder,
+	placeholder
 ].slice(0, 6);
