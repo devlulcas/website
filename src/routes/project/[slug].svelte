c0ff38c17a8cade58bbad2f594bc0909323d238f
devlulcas <lucasalvesrego.contato@gmail.com>
2022-01-23 23:30:43 -0300

    add: dynamic route for projects explanation

---
+++ src/routes/project/[slug].svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	const slug = $page.params.slug;
+	const apiUrl = `http://localhost:3000/api/project/${slug}.json`;
+	const slugTitle = slug.toUpperCase().replaceAll('_', ' ');
+
+	let project = getProject();
+
+	async function getProject() {
+		const response = await fetch(apiUrl);
+		const json = await response.json();
+		project = json.project;
+		return project;
+	}
+</script>
+
+<svelte:head>
+	<title>PROJECT - {slugTitle}</title>
+</svelte:head>
+
+<h1>{project.name}</h1>
