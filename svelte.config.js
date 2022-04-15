b818d733944bd544d5d4d148a9d3ca10d812ee79
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-15 20:58:10 -0300

    feat: path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,52 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+import { resolve } from "path";
 
-/** @type {import('@sveltejs/kit').Config} */
+const __dirname = resolve();
+
 const config = {
 	kit: {
+		// Config do vite para suportar path aliases
+		vite: {
+			resolve: {
+				alias: [
+					{
+						find: "$",
+						replacement: resolve(__dirname, "./src")
+					},
+					{
+						find: "$lib",
+						replacement: resolve(__dirname, "./src/lib")
+					},
+					{
+						find: "$models",
+						replacement: resolve(__dirname, "./src/models")
+					},
+					{
+						find: "$stores",
+						replacement: resolve(__dirname, "./src/stores")
+					},
+					{
+						find: "$assets",
+						replacement: resolve(__dirname, "./src/assets")
+					}
+				]
+			}
+		},
+		// Config do adapter para construir um site estático
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: "build",
+			assets: "build",
 			fallback: null,
 			precompress: false
 		}),
+		// Prerender para renderizar o conteúdo em tempo de compilação
 		prerender: {
 			default: true
 		}
 	},
 
+	// Config de pré-processadores como Sass, Stylus ou PostCSS
 	preprocess: [
 		preprocess({
 			postcss: true
