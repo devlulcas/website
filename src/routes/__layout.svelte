1d3be37f22f40f54d55c9cdba743d5e1c31e79c4
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-15 20:59:05 -0300

    style: format code

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import NavBar from '$lib/NavBar.svelte';
-	import '../app.css';
+	import NavBar from "$lib/NavBar.svelte";
+	import "../app.css";
 </script>
 
 <NavBar />
-<div>
-	<h1>PORTFÓLIO</h1>
-</div>
 <slot />
