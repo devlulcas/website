bf1854d1d4d670346e2558238db0fd0b4e306f99
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-15 21:02:34 -0300

    refactor: removing menu button from nav bar component

---
+++ src/lib/MenuButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let visible: boolean;
+</script>
+
+<button aria-hidden={visible} aria-label="open mobile menu" on:click />
+
+<style lang="postcss">
+	button {
+		display: none;
+		z-index: 10;
+
+		@media screen and (max-width: 100rem) {
+			display: block;
+			width: 4rem;
+		}
+	}
+</style>
