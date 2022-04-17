0234d6407fc42d44c86040220e86acc42f6c30ba
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-17 01:59:11 -0300

    feat: all sections

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Contact from "$/lib/sections/Contact.svelte";
+	import Projects from "$/lib/sections/Projects.svelte";
+	import Skills from "$/lib/sections/Skills.svelte";
+	import Start from "$/lib/sections/Start.svelte";
+</script>
+
+<Start />
+<Skills />
+<Projects />
+<Contact />
