ccac3e9de65e0ea0529c06dc1277847a0c3ce89e
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-22 23:05:09 -0300

    feat: horizontal scroll on drag

---
+++ src/lib/SideScroll.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let velocity: number;
+
+	let slider: HTMLElement;
+	let isDown = false;
+	let startX;
+	let scrollLeft;
+
+	function getMouseData(event: MouseEvent) {
+		isDown = true;
+		startX = event.pageX - slider.offsetLeft;
+		scrollLeft = slider.scrollLeft;
+	}
+
+	function moveSlider(event: MouseEvent) {
+		if (!isDown) return;
+		event.preventDefault();
+		const xPosition = event.pageX - slider.offsetLeft;
+		const walk = (xPosition - startX) * velocity;
+		slider.scrollLeft = scrollLeft - walk;
+	}
+
+	const setIsDown = () => (isDown = false);
+</script>
+
+<div
+	class="slider"
+	bind:this={slider}
+	on:mousedown={getMouseData}
+	on:mousemove={moveSlider}
+	on:mouseleave={setIsDown}
+	on:mouseup={setIsDown}
+>
+	<slot />
+</div>
+
+<style lang="postcss">
+	.slider {
+		overflow-x: scroll;
+		display: flex;
+		width: 100%;
+		height: 45vh;
+		padding: 1rem;
+		gap: 3rem;
+	}
+</style>
