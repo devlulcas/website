89fb48a6671132b35c95a4e177b36497e5eb25e1
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-23 19:57:14 -0300

    feat: submit button

---
+++ src/lib/Submit.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa/src/fa.svelte";
+</script>
+
+<div class="button-container">
+	<button>
+		Send e-mail
+		<Fa icon={faPaperPlane} />
+	</button>
+	<div class="divider" />
+</div>
+
+<style lang="postcss">
+	.button-container {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding-top: 2%;
+	}
+
+	button {
+		background: var(--primary);
+		color: var(--on-primary);
+		font-size: 1.2rem;
+		font-weight: 800;
+		padding: 1rem 1.5rem;
+		border: 0.3rem solid var(--on-primary);
+		border-radius: 2rem;
+		z-index: 1;
+		color: var(--on-primary);
+		cursor: pointer;
+		transition: 0.5s;
+		width: 30%;
+
+		@media screen and (max-width: 70rem) {
+			width: 80%;
+		}
+
+		&:hover {
+			color: var(--secondary);
+
+			& + .divider {
+				height: 1.2rem;
+				width: 80%;
+				background: var(--secondary);
+			}
+		}
+	}
+
+	.divider {
+		height: 0.5rem;
+		width: 60%;
+		border-radius: 5rem;
+		position: absolute;
+		transition: 0.5s;
+		background: var(--primary);
+	}
+</style>
