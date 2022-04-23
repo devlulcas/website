57df6c8a9a67d82a28b446351874759dca66b586
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-23 20:00:33 -0300

    feat: contact page

--- src/lib/sections/Contact.svelte
+++ src/lib/sections/Contact.svelte
@@ -1,3 +1,33 @@
-<section id="contact" class="piece piece--padding">
+<script lang="ts">
+	import Footer from "$lib/Footer.svelte";
+	import Form from "$lib/Form.svelte";
+</script>
+
+<section id="contact" class="piece contact">
 	<h2 class="piece__title">CONTACT</h2>
+
+	<div class="contact">
+		<img width="75px" src="/me_transparent.png" alt="me" />
+		<h3>Let's talk!</h3>
+		<Form />
+	</div>
+
+	<Footer />
 </section>
+
+<style lang="postcss">
+	.contact {
+		display: flex;
+		justify-content: space-between;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+
+	h3 {
+		font-weight: 600;
+		color: var(--special);
+		font-size: 2rem;
+		text-align: center;
+	}
+</style>
