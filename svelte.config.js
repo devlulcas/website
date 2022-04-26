91a785d67a5cda27371caf4fc078139575919254
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-26 00:39:11 -0300

    feat: new adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import { resolve } from "path";
 
 const __dirname = resolve();
