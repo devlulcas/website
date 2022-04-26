51cef878b5aff9405e13a8c1eab1a9daa8079a87
devlulcas <lucasalvesrego.contato@gmail.com>
2022-04-25 22:48:18 -0300

    refactor: make project list its own component

---
+++ src/lib/ProjectsList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { ProjectRepository } from "$/models/repositories/Project";
+	import Loading from "$lib/Loading.svelte";
+	import ProjectCard from "$lib/ProjectCard.svelte";
+	import SideScroll from "$lib/SideScroll.svelte";
+</script>
+
+{#await ProjectRepository.getAllProjects()}
+	<Loading />
+{:then projects}
+	<SideScroll velocity={2}>
+		{#each projects as project}
+			<ProjectCard {project} />
+		{/each}
+	</SideScroll>
+{/await}
