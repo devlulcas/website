e4d40791eaff82ab8b9b2dd52bf55fa438ecc2ce
devlulcas <lucasalvesrego.contato@gmail.com>
2022-07-10 23:00:36 -0300

    refactor: changed styles location

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import SvelteSeo from "svelte-seo";
 	import NavBar from "$lib/NavBar.svelte";
-	import "../app.css";
 
 	const title = "DEVLULCAS";
+
 	const url = "https://devlulcas.github.io";
+
 	const image = {
 		url: "https://www.example.com/images/og-image.jpg",
 		alt: "screenshot of devlulcas website"
 	};
+
 	const description =
 		"Software developer focused on web technologies like Typescript, PHP, Elixir, Svelte, React and Phoenix";
 </script>
