08748409346d53303007fdba4b849d1ab1c743ec
devlulcas <lucasalvesrego.contato@gmail.com>
2022-09-03 21:23:28 -0300

    feat: better aliases

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,12 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'$constants/*': 'src/constants/*'
+		}
 	}
 };
 
