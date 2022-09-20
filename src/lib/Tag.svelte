15c2ed95b4d567d68160a940cb3325bea8144a5d
lucas rego <lucasalvesrego.contato@gmail.com>
2022-09-20 15:23:29 -0300

    feat: tag component

---
+++ src/lib/Tag.svelte
@@ -0,0 +1,17 @@
+<!-- TYPESCRIPT -->
+<script lang="ts">
+</script>
+
+<!-- HTML -->
+
+<li><slot /></li>
+
+<!-- STYLES - SCSS -->
+<style lang="scss">
+	li {
+		padding: 0.5rem 1rem;
+		background: #000;
+		color: #fff;
+		border-radius: 0.25rem;
+	}
+</style>
