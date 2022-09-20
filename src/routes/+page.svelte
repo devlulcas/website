13da59ae18e2c7e42f0657195d89acfcc0422e28
lucas rego <lucasalvesrego.contato@gmail.com>
2022-09-20 15:24:54 -0300

    feat: show new content sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <!-- TYPESCRIPT -->
 <script lang="ts">
 	import Posts from '$modules/Posts.svelte';
+	import Projects from '$modules/Projects.svelte';
 	import Start from '$modules/Start.svelte';
 </script>
 
 <!-- HTML -->
 <Start />
 <Posts />
+<Projects />
