43335b781c5588746fe807421339dff6bc30b7b6
lucas rego <lucasalvesrego.contato@gmail.com>
2022-09-21 15:32:26 -0300

    feat: footer links

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,67 @@
+<!-- TYPESCRIPT -->
+<script lang="ts">
+	import Footer from '$lib/Footer.svelte';
+	import Heading from '$lib/Heading.svelte';
+</script>
+
+<!-- HTML -->
+<footer>
+	<ul>
+		<li>
+			<i>#</i>
+			<a href="/">GitHub</a>
+		</li>
+
+		<li>
+			<i>#</i>
+			<a href="/">LinkedIn</a>
+		</li>
+
+		<li>
+			<i>#</i>
+			<a href="/">Twitter</a>
+		</li>
+
+		<li>
+			<i>#</i>
+			<a href="/">Blog</a>
+		</li>
+	</ul>
+</footer>
+
+<!-- STYLES - SCSS -->
+<style lang="scss">
+	footer {
+		padding: 1rem;
+	}
+
+	ul {
+		display: flex;
+		gap: 1rem;
+	}
+
+	li {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		grid-template: 1rem;
+		backdrop-filter: blur(1rem);
+		border-radius: 0.5rem;
+		padding: 0.5rem;
+
+		&:hover {
+			transform: scale(1.1);
+		}
+	}
+
+	i {
+		padding-left: 0.5rem;
+	}
+
+	a {
+		padding: 0.5rem 1rem;
+		text-decoration: none;
+		color: #000;
+		font-weight: 600;
+	}
+</style>
