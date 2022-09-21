553f090ef67717d85ed2ccc3249b7279895c55c3
devlulcas <lucasalvesrego.contato@gmail.com>
2022-09-20 23:07:52 -0300

    feat: basic changes to timeline component

--- src/lib/Timelime.svelte
+++ src/lib/Timelime.svelte
@@ -10,6 +10,38 @@
 <!-- STYLES - SCSS -->
 <style lang="scss">
 	ol {
+		width: fit-content;
 		list-style: none;
+		display: flex;
+		gap: 2rem;
+		position: relative;
+
+		&::after {
+			content: '';
+			position: absolute;
+			width: 100%;
+			height: 0.5rem;
+			background: black;
+			border-radius: 0.5rem;
+			bottom: 0;
+		}
+	}
+
+	li {
+		display: flex;
+		height: 5rem;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.4rem;
+		font-weight: 600;
+
+		&::before {
+			content: '';
+			width: 0.5rem;
+			height: 100%;
+			background: black;
+			margin-right: 1rem;
+			border-radius: 0.5rem;
+		}
 	}
 </style>
