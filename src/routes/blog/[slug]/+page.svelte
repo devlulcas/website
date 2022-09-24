7dc77817330044ac0e06f1466fd57ab6013c2635
devlulcas <lucasalvesrego.contato@gmail.com>
2022-09-24 10:58:20 -0300

    feat: post page with placeholder styles

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,69 @@
+<!-- TYPESCRIPT -->
 <script lang="ts">
-	import type { PageServerLoad } from './$types';
+	import '$assets/styles/highlight.css';
+	import type { PageData } from './$types';
 
-	export let data: PageServerLoad;
+	export let data: PageData;
 </script>
 
-<h1>
-	{JSON.stringify(data)}
-</h1>
+<!-- HTML -->
+<pre>
+	{JSON.stringify(data.body.metadata)}
+</pre>
+
+<div class="content">
+	{@html data.body.content}
+</div>
+
+<!-- STYLES - SCSS -->
+<style lang="scss">
+	.content :global {
+		h1 {
+			color: blue;
+		}
+
+		h2 {
+			color: aqua;
+		}
+
+		h3 {
+			color: aquamarine;
+		}
+
+		p {
+			color: darkorange;
+		}
+
+		strong {
+			color: beige;
+		}
+
+		em {
+			color: brown;
+		}
+
+		del {
+			color: coral;
+		}
+
+		blockquote p {
+			color: rgb(0, 189, 16);
+		}
+
+		a {
+			color: red;
+		}
+
+		img {
+			width: 100%;
+		}
+
+		table {
+			color: darkorchid;
+		}
+
+		li {
+			color: fuchsia;
+		}
+	}
+</style>
