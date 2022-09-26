34627fcc8afbcc49f0412c5f5de80c7124d8d8fa
devlulcas <lucasalvesrego.contato@gmail.com>
2022-09-26 02:37:52 -0300

    fix: fullpath posts location

--- src/utils/posts/post-metadata.ts
+++ src/utils/posts/post-metadata.ts
@@ -1,9 +1,12 @@
 import fs from 'node:fs/promises';
 import { parseMarkdownFile } from './markdown-parser';
 import { sortPostsByDate } from './post-sort';
+import path from 'node:path';
+
+const POSTS_PATH = path.join(process.cwd(), '/posts');
 
 export async function getPostsMetadata() {
-	const list = await fs.readdir(process.cwd() + '/posts');
+	const list = await fs.readdir(POSTS_PATH);
 
 	const filesPromise = list.map((path) => {
 		return parseMarkdownFile(path);
