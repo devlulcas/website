d14d41c0b4a1b3a68fa1d00975a2b17ac90e6766
devlulcas <lucasalvesrego.contato@gmail.com>
2022-10-21 07:33:38 -0300

    feat: migrate project fetcher

---
+++ src/data/projects/project-fetcher.ts
@@ -0,0 +1,90 @@
+import type {
+	PinnedProjectGraphQlReturn,
+	ProjectGraphQlReturn
+} from '$commonTypes/project-graphql-return';
+
+import { GraphqlHelper } from '../graphql/helper';
+
+const url = process.env.GITHUB_GRAPHQL_ENDPOINT ?? 'https://api.github.com/graphql';
+const key = process.env.GITHUB_API_KEY;
+const client = new GraphqlHelper(url, `bearer ${key}`);
+
+export async function getPinnedProjects() {
+	const queryPinnedProjects = `
+	{
+		viewer {
+			pinnedItems(first: 4) {
+				edges {
+					node {
+						... on Repository {
+							name
+							url
+							description
+							homepageUrl
+							createdAt
+							languages(first: 10) {
+								nodes {
+									name
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}`;
+
+	try {
+		const pinnedProjectsResult = await client.runQuery<PinnedProjectGraphQlReturn>(
+			queryPinnedProjects
+		);
+
+		const pinnedProjectsNodes = pinnedProjectsResult.data.viewer.pinnedItems.edges;
+
+		const pinnedProjects = pinnedProjectsNodes.map(({ node }) => node);
+
+		return pinnedProjects;
+	} catch (error) {
+		return [];
+	}
+}
+
+export async function getAllProjects() {
+	const queryAllProjects = `
+	{
+		viewer {
+	  		repositories(
+					first: 50
+					orderBy: {field: CREATED_AT, direction: DESC}
+					privacy: PUBLIC
+	  		) {
+				edges {
+		  		node {
+						name
+						url
+						description
+						homepageUrl
+						createdAt
+						languages(first: 10) {
+							nodes {
+								name
+							}
+						}
+					}
+				}
+			}
+		}
+	}`;
+
+	try {
+		const allProjectsResult = await client.runQuery<ProjectGraphQlReturn>(queryAllProjects);
+
+		const allProjectNodes = allProjectsResult.data.viewer.repositories.edges;
+
+		const allProjects = allProjectNodes.map(({ node }) => node);
+
+		return allProjects;
+	} catch (error) {
+		return [];
+	}
+}
