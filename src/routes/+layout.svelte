5aa34328a3926e72fceee803d4c4d79d81c1bf2d
devlulcas <lucasalvesrego.contato@gmail.com>
2022-10-20 23:42:08 -0300

    refactor: im gonna try out astro

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '$assets/styles/reset.scss';
-	import '$assets/styles/global.scss';
-  import "$assets/styles/highlight.css"
-	import Header from '$lib/Header.svelte';
-</script>
-
-<Header />
-<slot />
