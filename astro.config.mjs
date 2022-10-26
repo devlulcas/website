05bb2e31c641499cdda94c6d3a9f15df7ea62e91
devlulcas <lucasalvesrego.contato@gmail.com>
2022-10-26 07:09:25 -0300

    deps: astro seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import localImageResolver from "./local-image-resolver";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import purgeCss from "astro-purgecss";
 
 export default defineConfig({
   vite: {
     plugins: localImageResolver,
   },
   site: "https://devlulcas-github-io.vercel.app/",
-  integrations: [svelte(), sitemap()],
+  integrations: [svelte(), purgeCss(), sitemap(), robotsTxt()],
 });
