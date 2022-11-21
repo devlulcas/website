0126f570f760430910eab07c2fb164889d121b93
devlulcas <lucasalvesrego.contato@gmail.com>
2022-11-21 01:34:28 -0300

    deps: removes purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import svelte from "@astrojs/svelte";
 import localImageResolver from "./local-image-resolver";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import purgeCss from "astro-purgecss";
 
 export default defineConfig({
   vite: {
     plugins: localImageResolver,
   },
   site: "https://devlulcas-github-io.vercel.app/",
-  integrations: [svelte(), purgeCss(), sitemap(), robotsTxt()],
+  integrations: [svelte(), sitemap(), robotsTxt()],
 });
