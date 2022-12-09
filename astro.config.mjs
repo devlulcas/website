4061dd22d110955bf2d023a19a8422b5167d308f
lucas rego <lucasalvesrego.contato@gmail.com>
2022-12-09 13:39:16 -0300

    build: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
 import localImageResolver from "./local-image-resolver";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: localImageResolver,
@@ -12,4 +13,5 @@ export default defineConfig({
   site: "https://devlulcas-github-io.vercel.app/",
   integrations: [svelte(), sitemap(), robotsTxt()],
   output: "server",
+  adapter: vercel(),
 });
