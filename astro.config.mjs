b02214963ae9d391fa6007d744a6ddaa657ca159
devlulcas <lucasalvesrego.contato@gmail.com>
2022-12-19 23:12:29 -0300

    build: removes vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import svelte from "@astrojs/svelte";
 import localImageResolver from "./local-image-resolver";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +11,4 @@ export default defineConfig({
   },
   site: "https://devlulcas-github-io.vercel.app/",
   integrations: [svelte(), sitemap(), robotsTxt()],
-  output: "server",
-  adapter: vercel(),
 });
