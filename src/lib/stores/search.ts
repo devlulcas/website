ecd417019b5b8671e4f96818baf87f5b21901764
devlulcas <lucasalvesrego.contato@gmail.com>
2023-01-15 09:56:47 -0300

    feat: feeling fuzzy

--- src/lib/stores/search.ts
+++ src/lib/stores/search.ts
@@ -1,18 +1,35 @@
+import { Fzf } from 'fzf';
 import { writable } from 'svelte/store';
 
-type Result = Record<PropertyKey, any>;
+type Result = Record<PropertyKey, any> & {
+	searchTerms: string;
+};
 
 export interface SearchStoreModel<T extends Result> {
 	data: T[];
 	filtered: T[];
 	query: string;
+	loadedFuzzyFinder: Fzf<T[]>;
 }
 
+// Creates a store that can be used to search through a list of data
 export const createSearchStore = <T extends Result>(data: T[]) => {
+	// This is working, but I'm not sure how to get the types to work correctly
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore - FzfOptions is not exported
+	const fzf = new Fzf(data, {
+		selector: (v) => {
+			return v.searchTerms;
+		},
+		casing: 'case-insensitive',
+		sort: true
+	});
+
 	const { subscribe, set, update } = writable<SearchStoreModel<T>>({
 		data,
 		filtered: data,
-		query: ''
+		query: '',
+		loadedFuzzyFinder: fzf
 	});
 
 	return {
@@ -22,10 +39,9 @@ export const createSearchStore = <T extends Result>(data: T[]) => {
 	};
 };
 
+// Uses the Fzf library to filter the data based on the query
 export const searchHandler = <T extends Result>(searchStore: SearchStoreModel<T>) => {
 	const searchTerm = searchStore.query.toLowerCase() || '';
 
-	searchStore.filtered = searchStore.data.filter((item) => {
-		return item.searchTerms.toLowerCase().includes(searchTerm);
-	});
+	searchStore.filtered = searchStore.loadedFuzzyFinder.find(searchTerm).map((found) => found.item);
 };
