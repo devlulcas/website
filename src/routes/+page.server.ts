d7b94c922657c3cf2ad40b0dcb8b2947afeed82b
devlulcas <lucasalvesrego.contato@gmail.com>
2023-03-17 07:09:31 -0300

    feat: remove contact handler

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,6 @@ import { fetchMoreProjects, fetchPinnedProjects } from '$lib/data/projects';
 import { themeHandler } from '$handlers/theme';
 import type { Actions, PageServerLoad } from './$types';
 import { langHandler } from '$handlers/lang';
-import { contactHandler } from '$handlers/contact';
 
 export const load: PageServerLoad = async () => {
 	const data = await fetchPosts({});
@@ -43,6 +42,5 @@ export const load: PageServerLoad = async () => {
 
 export const actions = {
 	setTheme: themeHandler,
-	setLang: langHandler,
-	contact: contactHandler
+	setLang: langHandler
 } satisfies Actions;
