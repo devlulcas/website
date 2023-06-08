1532626511d47c5ba83eaad8b926926dd0ac7ed4
devlulcas <lucasalvesrego.contato@gmail.com>
2023-06-08 20:46:28 -0300

    fix: blog posts path

--- src/routes/blog/[post]/+page.ts
+++ src/routes/blog/[post]/+page.ts
@@ -12,7 +12,7 @@ export const load: PageLoad = async ({ data }) => {
 		throw error(404, 'Post not found');
 	}
 
-	const component = await import(`./../../../../../posts/${postPath}/index.md`);
+	const component = await import(`./../../../../posts/${postPath}/index.md`);
 
 	return {
 		post: data.post,
