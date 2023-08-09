4577a70e879ac5aeec845daa35db1b7b8bedd8bd
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-09 14:59:32 -0300

    feat: better feedback on form submition

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,28 +7,30 @@ import { zfd } from 'zod-form-data';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const featuredPosts = await fetchPosts();
+	const posts = await fetchPosts();
+
+	const featuredPost = posts[0];
+
+	const recentPosts = posts.splice(1, 3);
 
 	const featuredProjects = fetchFeaturedProjects();
 
-	const projects = await fetchProjects().catch((e) => {
-		console.error(e);
-		return [];
-	});
+	const projects = await fetchProjects();
 
-	return { featuredProjects, projects, featuredPosts: featuredPosts.slice(0, 3) };
+	return { featuredProjects, projects, recentPosts, featuredPost };
 };
 
-const contactSchema = zfd.formData({
-	name: zfd.text(),
-	email: zfd.text(z.string().email()),
-	zummm: zfd.text(z.string().nullish()),
-	message: zfd.text()
-});
-
 export const actions: Actions = {
-	default: async (event) => {
+	// Send a message from the contact form
+	sendMessage: async (event) => {
 		const formData = await event.request.formData();
+
+		const contactSchema = zfd.formData({
+			name: zfd.text(),
+			email: zfd.text(z.string().email()),
+			avocado: zfd.text(z.string().nullish()),
+			message: zfd.text()
+		});
 
 		const formDataResult = contactSchema.safeParse(formData);
 
@@ -37,17 +39,25 @@ export const actions: Actions = {
 			return fail(400, { message: 'Invalid form data' });
 		}
 
-		if (formDataResult.data.zummm) {
+		if (formDataResult.data.avocado) {
 			console.log('bot detected');
 			return fail(400, { message: 'Invalid form data' });
 		}
 
-		sendEmailNotification({
+		const result = await sendEmailNotification({
 			content: formDataResult.data.message,
 			from: formDataResult.data.email,
 			subject: `New message from ${formDataResult.data.name} on your website`
 		});
+
+		if (result.success === false) {
+			console.error(result.error);
+			return fail(500, { message: 'Error sending email' });
+		}
+
+		return { message: 'Message sent' };
 	},
+	// Change the users theme based on the url and save that state in a cookie
 	setTheme: async ({ cookies, url }) => {
 		const theme = url.searchParams.get('theme');
 		const redirectTo = url.searchParams.get('redirectTo') ?? '/';
@@ -60,6 +70,24 @@ export const actions: Actions = {
 				path: '/',
 				maxAge: fullYear
 			});
+		}
+
+		if (doNotRedirect) return;
+
+		if (redirectTo && redirectTo.startsWith('/')) {
+			throw redirect(303, redirectTo);
+		}
+
+		throw redirect(303, '/');
+	},
+	// Change the users language based on the url and save that state in a cookie
+	setLocale: async ({ cookies, url }) => {
+		const currentLanguageSetting = url.searchParams.get('lang');
+		const redirectTo = url.searchParams.get('redirectTo') ?? '/';
+		const doNotRedirect = url.searchParams.get('doNotRedirect') === 'true';
+
+		if (currentLanguageSetting) {
+			cookies.set('lang', currentLanguageSetting);
 		}
 
 		if (doNotRedirect) return;
