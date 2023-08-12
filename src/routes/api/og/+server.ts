7521fa18e3488ef15be20608b79a7a58ef348da1
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-12 02:41:35 -0300

    feat: open graph image gen

---
+++ src/routes/api/og/+server.ts
@@ -0,0 +1,14 @@
+import { ogHandler } from '$/lib/server/seo/og';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ url }) => {
+	const text = url.searchParams.get('text') || 'Hello world!';
+
+	const ogImage = await ogHandler({ text });
+
+	return new Response(ogImage, {
+		headers: {
+			'Content-Type': 'image/png'
+		}
+	});
+};
