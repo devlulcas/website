77fa48de3293b36821f002dc5a4b0294a5a88d2f
devlulcas <lucasalvesrego.contato@gmail.com>
2023-08-12 02:10:10 -0300

    feat: skill card size variation

--- src/lib/components/skill-card/skill-card.svelte
+++ src/lib/components/skill-card/skill-card.svelte
@@ -7,13 +7,13 @@
 <a
 	href={skill.url}
 	title={skill.name}
-	class="relative flex items-center justify-center border bg-dark/10 dark:bg-white/10 rounded w-28 aspect-square"
+	class="relative flex items-center justify-center dark:border bg-dark/10 dark:bg-white/10 rounded w-20 lg:w-28 aspect-square"
 >
 	<div class="absolute inset-0 w-full flex items-center justify-center h-full backdrop-blur-md z-10">
-		<img src={skill.image} alt={skill.name} class="w-24 md:w-12 aspect-square" />
+		<img src={skill.image} alt={skill.name} class="w-1/2 aspect-square" />
 	</div>
 
-	<img src={skill.image} alt={skill.name} class="animate-pulse w-26 md:w-14 aspect-square opacity-75" />
+	<img src={skill.image} alt={skill.name} class="animate-pulse w-1/2 aspect-square opacity-75" />
 </a>
 
 <style lang="postcss">
