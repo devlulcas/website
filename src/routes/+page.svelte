c6de8019f1839bce36bfc3c6c6b60c920d20a546
devlulcas <lucasalvesrego.contato@gmail.com>
2023-10-05 00:12:58 -0300

    fix: improve a11y

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 
 		<FeaturedPostCard post={data.featuredPost} />
 
-		<div class="h-[1px] lc-line w-full bg-brand-500 dark:bg-brand-800 rounded-full my-4" />
+		<div aria-hidden="true" class="h-[1px] lc-line w-full bg-brand-500 dark:bg-brand-800 rounded-full my-4" />
 
 		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
 			{#each data.recentPosts as recentPost}
