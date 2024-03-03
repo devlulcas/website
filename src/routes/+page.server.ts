5399eda247edd6ab0e412077d55eec0b082e0331
devlulcas <lucasalvesrego.contato@gmail.com>
2024-03-02 23:43:52 -0300

    feat: fixing up the contact form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import { getPosts } from '$/lib/server/posts/services/get-posts';
 import { getFeaturedProjects } from '$/lib/server/projects/services/get-feature-projects';
 import { getProjects } from '$/lib/server/projects/services/get-projects';
-import { error, redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import { sql } from '@vercel/postgres';
 import type { Actions, PageServerLoad } from './$types';
 
@@ -28,7 +28,13 @@ export const actions: Actions = {
     const formValueMessage = formData.get('message');
 
     if (!formValueName || !formValueEmail || !formValueMessage) {
-      throw error(400, 'Missing form data');
+      return fail(400, {
+        type: 'failure',
+        message: {
+          ptBr: 'Por favor, preencha todos os campos',
+          en: 'Please fill all the fields',
+        },
+      });
     }
 
     const name = formValueName.toString();
@@ -37,9 +43,24 @@ export const actions: Actions = {
 
     try {
       await sql`INSERT INTO messages (name, email, message) VALUES (${name}, ${email}, ${message})`;
-    } catch (err) {
-      console.error(err);
-      throw error(500, 'Failed to send message');
+
+      return {
+        type: 'success',
+        message: {
+          ptBr: 'Mensagem enviada com sucesso!',
+          en: 'Message sent successfully!',
+        },
+      };
+    } catch (error) {
+      console.error(error);
+
+      return fail(500, {
+        type: 'failure',
+        message: {
+          ptBr: 'Erro ao enviar a mensagem',
+          en: 'Error sending the message',
+        },
+      });
     }
   },
   // Change the users theme based on the url and save that state in a cookie
