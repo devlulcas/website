7ebe401a217a21fc39650a5d8aa08dcbd4541f00
devlulcas <lucasalvesrego.contato@gmail.com>
2024-10-07 01:00:52 -0300

    feat: nuggets cli

---
+++ cli/lib.ts
@@ -0,0 +1,103 @@
+const backgroundColors = {
+  black: '40',
+  red: '41',
+  green: '42',
+  yellow: '43',
+  blue: '44',
+  magenta: '45',
+  cyan: '46',
+  white: '47',
+} as const;
+type BackgroundColor = keyof typeof backgroundColors;
+
+const textColors = {
+  black: '30',
+  red: '31',
+  green: '32',
+  yellow: '33',
+  blue: '34',
+  magenta: '35',
+  cyan: '36',
+  white: '37',
+} as const;
+type TextColor = keyof typeof textColors;
+
+const styles = {
+  reset: '0',
+  bold: '1',
+  dim: '2',
+  italic: '3',
+  underline: '4',
+  inverse: '7',
+  hidden: '8',
+  strikethrough: '9',
+} as const;
+type Style = keyof typeof styles;
+
+class TextBuilder {
+  private text: string;
+
+  constructor(text: string) {
+    this.text = text;
+  }
+
+  color(color: TextColor): this {
+    this.text = `\x1b[${textColors[color]}m${this.text}\x1b[0m`;
+    return this;
+  }
+
+  background(color: BackgroundColor): this {
+    this.text = `\x1b[${backgroundColors[color]}m${this.text}\x1b[0m`;
+    return this;
+  }
+
+  style(style: Style): this {
+    this.text = `\x1b[${styles[style]}m${this.text}\x1b[0m`;
+    return this;
+  }
+
+  build(): string {
+    return this.text;
+  }
+}
+
+export const t = (text: string) => new TextBuilder(text);
+
+import readline from 'node:readline';
+
+export const clear = () => {
+  console.clear()
+}
+
+export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+export const read = (question: string) => new Promise<string>(resolve => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  const formated = t('>>> ' + question + ': ').color('cyan').style('bold').build()
+  rl.question(formated, (answer) => {
+    rl.close()
+    resolve(answer)
+  })
+})
+
+
+export const confirm = (question: string) => new Promise<boolean>(resolve => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  const formated = t('>>> ' + question + ' (y/n): ').color('cyan').style('bold').build()
+  rl.question(formated, (answer) => {
+    rl.close()
+    resolve(answer === 'y')
+  })
+})
+
+export const log = (message: TextBuilder | string) => {
+  console.log(message instanceof TextBuilder ? message.build() : message)
+}
---
+++ cli/nugget.ts
@@ -0,0 +1,64 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import * as tr from './lib'
+
+
+try {
+  tr.clear()
+  const title = await tr.read('Title')
+
+  tr.log(tr.t('Title: ').color('cyan').style('bold').build() + title)
+  const slug = await tr.read('Slug')
+
+  const replaceChars = (slug: string) => {
+    const s = slug.toLowerCase().replace(/ /g, '-').replace(/[^a-z0-9-]/g, '')
+    if (s !== slug) {
+      tr.log(tr.t('Slug: ').color('yellow').style('bold').build() + s)
+      return s
+    }
+    return slug
+  }
+
+  let newSlug = replaceChars(slug)
+
+  const confirmSlug = await tr.confirm('Do you want to use this slug?')
+
+  if (!confirmSlug) {
+    newSlug = await tr.read('Slug')
+    replaceChars(newSlug)
+  }
+
+  const content = {
+    title,
+    slug: newSlug,
+    year: new Date().getFullYear(),
+  }
+
+  const filepath = path.join(process.cwd(), 'content', 'nuggets', content.year.toString(), content.slug, 'index.md')
+  tr.log(tr.t('Filepath: ').color('cyan').style('bold').build() + filepath)
+
+  const text = `# ${content.title.toUpperCase()}\n\n`
+  tr.log(tr.t('Content:').color('cyan').style('bold').build())
+  tr.log(text)
+
+  const confirmSave = await tr.confirm('Do you want to save this content?')
+
+  if (confirmSave) {
+    tr.log(tr.t('Saving...').color('green').style('bold').build())
+
+    if (!fs.existsSync(path.dirname(filepath))) {
+      fs.mkdirSync(path.dirname(filepath), { recursive: true })
+      tr.log(tr.t('Saved!').color('green').style('bold').build())
+      fs.writeFileSync(filepath, text)
+
+      tr.log(tr.t('File created:').color('cyan').style('bold').build())
+      tr.log(filepath)
+    } else {
+      tr.log(tr.t('Directory already exists.').color('yellow').style('bold').build())
+    }
+  } else {
+    tr.log(tr.t('Canceled!').color('red').style('bold').build())
+  }
+} catch (error) {
+  console.error(error)
+}
