d6d6d0c7d315fa618d47aeb3d149d366f0582f09
devlulcas <lucasalvesrego.contato@gmail.com>
2024-12-30 15:34:19 -0300

    fix: og images

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -4,6 +4,7 @@ import inter from '$lib/assets/fonts/inter-bold.ttf';
 import { Resvg } from '@resvg/resvg-js';
 import satori from 'satori';
 import { html as toReactNode } from 'satori-html';
+import { render } from 'svelte/server';
 import type { RequestHandler } from './$types';
 import SvelteOG from './og.svelte';
 
@@ -16,20 +17,23 @@ export const GET: RequestHandler = async ({ url }) => {
 	const searchParams = url.searchParams;
 
 	const text = searchParams.get('text') || website.title;
-	const thumb = searchParams.get('thumb') || null;
+	const thumb = searchParams.get('thumb') || website.image;
 	const tags = searchParams.getAll('tag') ?? [];
-	const isMain = searchParams.get('main') === 'true';
 
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
-	const result = SvelteOG.render({
-		text,
-		thumb,
-		tags,
-		isMain
+	const result = render(SvelteOG, {
+		props: {
+			text,
+			thumb,
+			tags,
+		}
 	});
 
-	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+	console.log(result.head);
+
+
+	const element = toReactNode(`${result.html}`);
 
 	const svg = await satori(element, {
 		fonts: [
--- src/routes/api/og/og.svelte
+++ src/routes/api/og/og.svelte
@@ -19,18 +19,37 @@
 	);
 </script>
 
-<div class="container">
-	<div class="card">
-		<img src={thumb} alt={text} width="800" />
-		<div class="content">
-			<p>{text}</p>
+<div
+	style="display: flex; justify-content: center; align-items: center; height: 630px; width: 1200px; background-color: transparent;"
+>
+	<div
+		style="display: flex; background-color: #171717; width: 100%; height: 100%; border: 1px solid #262626; border-radius: 10px; overflow: hidden;"
+	>
+		<img
+			style="display: flex; background-color: #262626; height: 100%; object-fit: cover;"
+			src={thumb}
+			alt={text}
+			width="800"
+		/>
+		<div
+			style="display: flex; flex-direction: column; justify-content: space-between; padding: 20px;"
+		>
+			<p
+				style="display: flex; justify-content: center; font-size: 35px; width: 300px; margin: 5px; color: #fafafa;"
+			>
+				{text}
+			</p>
 
 			{#if tags}
-				<div class="tags">
+				<div style="display: flex; width: 300px; flex-direction: column;">
 					{#each tagsInPacks as tagsPack}
-						<div>
+						<div style="display: flex;">
 							{#each tagsPack as tag}
-								<span>{tag}</span>
+								<span
+									style="display: flex; justify-content: center; align-items: center; padding: 5px 10px; margin: 5px; border-radius: 5px; background-color: #262626; color: #fafafa;"
+								>
+									{tag}
+								</span>
 							{/each}
 						</div>
 					{/each}
@@ -39,63 +58,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	div {
-		display: flex;
-	}
-
-	.container {
-		justify-content: center;
-		align-items: center;
-		height: 630px;
-		width: 1200px;
-		background-color: transparent;
-	}
-
-	.card {
-		background-color: #171717;
-		width: 100%;
-		height: 100%;
-		border: 1px solid #262626;
-		border-radius: 10px;
-		overflow: hidden;
-	}
-
-	.content {
-		flex-direction: column;
-		justify-content: space-between;
-		padding: 20px;
-	}
-
-	.tags {
-		width: 300px;
-		flex-direction: column;
-	}
-
-	img {
-		background-color: #262626;
-		height: 100%;
-		object-fit: cover;
-	}
-
-	p {
-		display: flex;
-		justify-content: center;
-		font-size: 35px;
-		width: 300px;
-		margin: 5px;
-		color: #fafafa;
-	}
-
-	span {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		padding: 5px 10px;
-		margin: 5px;
-		border-radius: 5px;
-		background-color: #262626;
-		color: #fafafa;
-	}
-</style>
